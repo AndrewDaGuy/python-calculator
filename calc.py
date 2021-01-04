f72f2b14dced84c6f079ac586f2e9053d6e1835e
Andrew Hung <66189242+AndrewDaGuy@users.noreply.github.com>
2021-01-03 19:12:57 -0800

    :boom: Add code

--- calc.py
+++ calc.py
@@ -1,1 +1,36 @@
+print("Select an operation!")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+print("------------")
 
+while True:
+    choice = input("Input > ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("First number > "))
+        num2 = float(input("Second number > "))
+        result = ""
+
+        if choice == '1':
+            result = num1 + num2
+        elif choice == '2':
+            result = num1 - num2
+        elif choice == '3':
+            result = num1 * num2
+        elif choice == '4':
+            if num1 == 0:
+                print("Cannot divide by 0 :/")
+                break
+
+            if num2 == 0:
+                print("Cannot divide by 0 :/")
+                break
+
+            result = num1 / num2
+        print(" ")
+        print("Result >>> ", result)
+        break
+    else:
+        print("Invalid operation. :/")
